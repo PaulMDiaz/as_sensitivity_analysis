b136d32ca501fd8e69aa338cf6d89645de52265c
Paul M Diaz <pdiaz@mymail.mines.edu>
2015-08-10 23:55:10 -0400

    Added .mat files used in figure generation, and slight modification of the error figure printing script.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # as_sensitivity_analysis
 This repository contains all code for the numerical experiments performed in the global sensitivity analysis
-from active subspaces paper.
+from active subspaces paper. To generate the figures of this paper run the script error_figure_printing.m
 Authors: Paul Diaz, Dr. Paul Constantine
 email: pdiaz@mymail.mines.edu
-Date: August 8th 2015 
-Please note that these scripts require the parameterized matrix package for matlab freely available at:
+Date: August 10 2015 
+Part of this code uses the parameterized matrix package; the entire package is freely available at URL 
 http://www.mathworks.com/matlabcentral/fileexchange/29228-pmpack-parameterized-matrix-package
+
+
+
--- error_figure_printing.m
+++ error_figure_printing.m
@@ -11,12 +11,13 @@ h = xlabel('M');
 f = ylabel('Relative Error');
 set(h,'Interpreter','latex', 'FontSize', 12);
 set(f,'Interpreter','latex', 'FontSize', 12);
-legend('1','2','3','4','5','6','7','Location','BestOutside');
+legend('1','2','3','4','5','6','7','Location','SouthWest');
 ylim([1e-4 1e6]);
-
-print('err_tsi_piston','-dpdf','-r300');
-close;
-
+%print('err_tsi_piston','-dpdf','-r300');
+%close;
+%%
+close all;clear all; clear; clc;
+load('piston_sobol_err')
 figure();
 loglog(Nsamples, mean(sobol_std_err,3),'o-','LineWidth',2);
 axis square; grid on;
@@ -24,10 +25,10 @@ h=xlabel('M');
 f=ylabel('Standard Deviation');
 set(h,'Interpreter','latex', 'FontSize', 12);
 set(f,'Interpreter','latex', 'FontSize', 12);
-legend('1','2','3','4','5','6','7','Location','BestOutside');
+legend('1','2','3','4','5','6','7','Location','SouthWest');
 ylim([1e-10 1e-1]);
-print('std_err_tsi_piston','-dpdf','-r300');
-close;
+%print('std_err_tsi_piston','-dpdf','-r300');
+%close;
 %%
 close all;clear all; clear; clc;
 load('otl_sobol_err')
@@ -38,11 +39,13 @@ h=xlabel('M');
 f=ylabel('Relative Error');
 set(h,'Interpreter','latex', 'FontSize', 12);
 set(f,'Interpreter','latex', 'FontSize', 12);
-legend('1','2','3','4','5','6','Location','BestOutside');
+legend('1','2','3','4','5','6','Location','SouthWest');
 ylim([1e-4 1e8]);
-print('err_tsi_otl','-dpdf','-r300');
-close;
-
+%print('err_tsi_otl','-dpdf','-r300');
+%close;
+%%
+close all;clear all; clear; clc;
+load('otl_sobol_err')
 figure();
 loglog(Nsamples, mean(sobol_std_err,3),'o-','LineWidth',2);
 axis square; grid on;
@@ -50,10 +53,10 @@ h=xlabel('M');
 f=ylabel('Standard Deviation');
 set(h,'Interpreter','latex', 'FontSize', 12);
 set(f,'Interpreter','latex', 'FontSize', 12);
-legend('1','2','3','4','5','6','Location','BestOutside');
+legend('1','2','3','4','5','6','Location','SouthWest');
 ylim([1e-11 1e1]);
-print('std_err_tsi_otl','-dpdf','-r300');
-close;
+%print('std_err_tsi_otl','-dpdf','-r300');
+%close;
 
 %%
 close all;clear all; clear; clc;
@@ -65,11 +68,13 @@ h=xlabel('M');
 f=ylabel('Relative Error');
 set(h,'Interpreter','latex', 'FontSize', 12);
 set(f,'Interpreter','latex', 'FontSize', 12);
-legend('1','2','3','4','5','6','7','Location','BestOutside');
+legend('1','2','3','4','5','6','7','Location','NorthWest');
 ylim([1e-4 1e6]);
-print('err_dbsm_piston','-dpdf','-r300');
-close;
-
+%print('err_dbsm_piston','-dpdf','-r300');
+%close;
+%%
+close all;clear all; clear; clc;
+load('piston_dgsm_err')
 figure();
 loglog(Nsamples, mean(dgsm_std_err,3),'o-','LineWidth',2);
 axis square; grid on;
@@ -77,10 +82,10 @@ h=xlabel('M');
 f=ylabel('Standard Deviation');
 set(h,'Interpreter','latex', 'FontSize', 12);
 set(f,'Interpreter','latex', 'FontSize', 12);
-legend('1','2','3','4','5','6','7','Location','BestOutside');
+legend('1','2','3','4','5','6','7','Location','SouthWest');
 ylim([1e-10 1e-1]);
-print('std_err_dbsm_piston','-dpdf','-r300');
-close;
+%print('std_err_dbsm_piston','-dpdf','-r300');
+%close;
 %%
 close all;clear all; clear; clc;
 load('otl_dgsm_err')
@@ -91,11 +96,13 @@ h=xlabel('M');
 f=ylabel('Relative Error');
 set(h,'Interpreter','latex', 'FontSize', 12);
 set(f,'Interpreter','latex', 'FontSize', 12);
-legend('1','2','3','4','5','6','Location','BestOutside');
+legend('1','2','3','4','5','6','Location','NorthWest');
 ylim([1e-4 1e8]);
-print('err_dbsm_otl','-dpdf','-r300');
-close;
-
+%print('err_dbsm_otl','-dpdf','-r300');
+%close;
+%%
+close all;clear all; clear; clc;
+load('otl_dgsm_err')
 figure();
 loglog(Nsamples, mean(dgsm_std_err,3),'o-','LineWidth',2);
 axis square; grid on;
@@ -103,10 +110,10 @@ h=xlabel('M');
 f=ylabel('Standard Deviation');
 set(h,'Interpreter','latex', 'FontSize', 12);
 set(f,'Interpreter','latex', 'FontSize', 12);
-legend('1','2','3','4','5','6','Location','BestOutside');
+legend('1','2','3','4','5','6','Location','SouthWest');
 ylim([1e-11 1e1]);
-print('figs/std_err_dbsm_otl','-dpdf','-r300');
-close;
+%print('figs/std_err_dbsm_otl','-dpdf','-r300');
+%close;
 %%
 close all;clear all; clear; clc;
 load('piston_AS_err')
@@ -117,11 +124,13 @@ h=xlabel('M');
 f=ylabel('Relative Error');
 set(h,'Interpreter','latex', 'FontSize', 12);
 set(f,'Interpreter','latex', 'FontSize', 12);
-legend('1','2','3','4','5','6','7','Location','BestOutside');
+legend('1','2','3','4','5','6','7','Location','NorthWest');
 ylim([1e-4 1e6]);
-print('err_as_piston','-dpdf','-r300');
-close;
-
+%print('err_as_piston','-dpdf','-r300');
+%close;
+%%
+close all;clear all; clear; clc;
+load('piston_AS_err')
 figure();
 loglog(Nsamples, mean(as_std_err,3),'o-','LineWidth',2);
 axis square; grid on;
@@ -129,10 +138,10 @@ h=xlabel('M');
 f=ylabel('Standard Deviation');
 set(h,'Interpreter','latex', 'FontSize', 12);
 set(f,'Interpreter','latex', 'FontSize', 12);
-legend('1','2','3','4','5','6','7','Location','BestOutside');
+legend('1','2','3','4','5','6','7','Location','NorthEast');
 ylim([1e-10 1e-1]);
-print('std_err_as_piston','-dpdf','-r300');
-close;
+%print('std_err_as_piston','-dpdf','-r300');
+%close;
 %%
 close all;clear all; clear; clc;
 load('otl_AS_err')
@@ -143,11 +152,13 @@ h=xlabel('M');
 f=ylabel('Relative Error');
 set(h,'Interpreter','latex', 'FontSize', 12);
 set(f,'Interpreter','latex', 'FontSize', 12);
-legend('1','2','3','4','5','6','Location','BestOutside');
+legend('1','2','3','4','5','6','Location','NorthWest');
 ylim([1e-4 1e8]);
-print('err_as_otl','-dpdf','-r300');
-close;
-
+%print('err_as_otl','-dpdf','-r300');
+%close;
+%%
+close all;clear all; clear; clc;
+load('otl_AS_err')
 figure();
 loglog(Nsamples, mean(as_std_err,3),'o-','LineWidth',2);
 axis square; grid on;
@@ -155,7 +166,7 @@ h=xlabel('M');
 f=ylabel('Standard Deviation');
 set(h,'Interpreter','latex', 'FontSize', 12);
 set(f,'Interpreter','latex', 'FontSize', 12);
-legend('1','2','3','4','5','6','Location','BestOutside');
+legend('1','2','3','4','5','6','Location','NorthEast');
 ylim([1e-11 1e1]);
-print('std_err_as_otl','-dpdf','-r300');
-close;
+%print('std_err_as_otl','-dpdf','-r300');
+%close;
